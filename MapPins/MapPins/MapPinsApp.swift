6bfc06b084ba94f8ff96a18b6810213daf589333
thomas <lacan.thomas@gmail.com>
2023-01-12 22:07:08 +0100

    Project setup
    SPM SwilftLint
    SPM SwilftGen + config for Assets/Fonts/Colors/Lottie
    SPM Lottie

---
+++ MapPins/MapPins/MapPinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPinsApp.swift
+//  MapPins
+//
+//  Created by thomas lacan on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapPinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapPins/MapPins/resources/generated/Lotties.swift
@@ -0,0 +1,28 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+import Lottie
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// MARK: - Lottie Files Enum
+
+// swiftlint:disable identifier_name line_length number_separator type_body_length
+internal enum Lottie {
+  internal static let map = LottieAnimation.named("map")!
+  internal static let people = LottieAnimation.named("people")!
+  internal static let rating = LottieAnimation.named("rating")!
+  internal static let yummy = LottieAnimation.named("yummy")!
+}
+// swiftlint:enable identifier_name line_length number_separator type_body_length
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    Bundle(for: BundleToken.self)
+  }()
+}
+// swiftlint:enable convenience_type
+
